e97dcde509b108056108bc1641dd33dccc941a2f
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-10-30 15:05:53 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,17 +57,49 @@ exports.createPages = async ({ graphql, actions }) => {
     const recipes = result2.data.Drupal.nodeRecipes.edges.map(edge => edge.node)
     recipes.forEach(recipedata => {
       createPage({
-        path: (${__dirname}/one.js),
-        component: path.resolve('${__dirname}/one.js'),
-        /*component: path.resolve('src/pages/recipe/two.js'),
+        const recipeRoutes = [
+    {
+        path: recipedata.path + '/one',
+        component: path.resolve('src/pages/recipe/one.js'),
+    },
+    {
+        path: recipedata.path + '/two',
+        component: path.resolve('src/pages/recipe/two.js'),
+    },
+    {
+        path: recipedata.path + '/three',
         component: path.resolve('src/pages/recipe/three.js'),
+    },
+    {
+        path: recipedata.path + '/four',
         component: path.resolve('src/pages/recipe/four.js'),
+    },
+    {
+        path: recipedata.path + '/five',
         component: path.resolve('src/pages/recipe/five.js'),
+    },
+    {
+        path: recipedata.path + '/six',
         component: path.resolve('src/pages/recipe/six.js'),
+    },
+    {
+        path: recipedata.path + '/seven',
         component: path.resolve('src/pages/recipe/seven.js'),
+    },
+    {
+        path: recipedata.path + '/eight',
         component: path.resolve('src/pages/recipe/eight.js'),
+    },
+    {
+        path: recipedata.path + '/nine',
         component: path.resolve('src/pages/recipe/nine.js'),
-        component: path.resolve('src/pages/recipe/ten.js'),*/
+    },
+    {
+        path: recipedata.path + '/ten',
+        component: path.resolve('src/pages/recipe/ten.js'),
+    }
+];
+
         context: {
           RecipeId: recipedata.id,
         },

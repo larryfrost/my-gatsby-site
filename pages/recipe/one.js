86f2db381bb7ddce6ba133dc4af72bcb66534456
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-06 14:14:12 -0500

    Update one.js

--- pages/recipe/one.js
+++ pages/recipe/one.js
@@ -3,13 +3,11 @@ import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 import { graphql } from 'gatsby';
 
-const RecipePage = ({ data }) => {
-  const recipes = data.Drupal.nodeRecipes.edges.map(edge => edge.node);
-  const filteredRecipe = recipes.find(recipe => recipe.title === "SUUPRDEEP mediterranean quiche");
-
+const RecipePage = ({ pageContext }) => {
+  const { recipe } = pageContext
   return (
     <Layout>
-      <h1>{filteredRecipe.title}</h1>
+      <h1>{recipe.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: filteredRecipe.summary.processed }}></div>
       <div dangerouslySetInnerHTML={{ __html: filteredRecipe.recipeInstruction.value }}></div>
     </Layout>

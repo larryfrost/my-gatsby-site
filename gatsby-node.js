49eb2b3c6d2a4110f2f50d37aa60d4183c02c027
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-06 12:45:16 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,14 +18,14 @@ exports.createPages = async ({ graphql, actions }) => {
     }
     `);
 
-    const articleTemone =  path.resolve('src/pages/article/articles/one.js');
-    const articleTemtwo =  path.resolve('src/pages/article/articles/two.js');
-    const articleTemthree =  path.resolve('src/pages/article/articles/three.js');
-    const articleTemfour =  path.resolve('src/pages/article/articles/four.js');
-    const articleTemfive =  path.resolve('src/pages/article/articles/five.js');
-    const articleTemsix =  path.resolve('src/pages/article/articles/six.js');
-    const articleTemseven =  path.resolve('src/pages/article/articles/seven.js');
-    const articleTemeight =  path.resolve('src/pages/article/articles/eight.js');
+    const articleTemone =  path.resolve('src/pages/article/articles/one.js')
+    const articleTemtwo =  path.resolve('src/pages/article/articles/two.js')
+    const articleTemthree =  path.resolve('src/pages/article/articles/three.js')
+    const articleTemfour =  path.resolve('src/pages/article/articles/four.js')
+    const articleTemfive =  path.resolve('src/pages/article/articles/five.js')
+    const articleTemsix =  path.resolve('src/pages/article/articles/six.js')
+    const articleTemseven =  path.resolve('src/pages/article/articles/seven.js')
+    const articleTemeight =  path.resolve('src/pages/article/articles/eight.js')
     const articles = result.data.Drupal.nodeArticles.nodes;
     articles.forEach(articledata => {
       createPage({
@@ -57,16 +57,16 @@ exports.createPages = async ({ graphql, actions }) => {
     `);
 
 
-    const recipeTemone =  path.resolve('src/pages/recipe/one.js');
-    const recipeTemtwo =  path.resolve('src/pages/recipe/two.js');
-    const recipeTemthree =  path.resolve('src/pages/recipe/three.js');
-    const recipeTemfour =  path.resolve('src/pages/recipe/four.js');
-    const recipeTemfive =  path.resolve('src/pages/recipe/five.js');
-    const recipeTemsix =  path.resolve('src/pages/recipe/six.js');
-    const recipeTemseven =  path.resolve('src/pages/recipe/seven.js');
-    const recipeTemeight =  path.resolve('src/pages/recipe/eight.js');
-    const recipeTemnine =  path.resolve('src/pages/recipe/nine.js');
-    const recipeTemten =  path.resolve('src/pages/recipe/ten.js');
+    const recipeTemone =  path.resolve('src/pages/recipe/one.js')
+    const recipeTemtwo =  path.resolve('src/pages/recipe/two.js')
+    const recipeTemthree =  path.resolve('src/pages/recipe/three.js')
+    const recipeTemfour =  path.resolve('src/pages/recipe/four.js')
+    const recipeTemfive =  path.resolve('src/pages/recipe/five.js')
+    const recipeTemsix =  path.resolve('src/pages/recipe/six.js')
+    const recipeTemseven =  path.resolve('src/pages/recipe/seven.js')
+    const recipeTemeight =  path.resolve('src/pages/recipe/eight.js')
+    const recipeTemnine =  path.resolve('src/pages/recipe/nine.js')
+    const recipeTemten =  path.resolve('src/pages/recipe/ten.js')
     const recipes = result2.data.Drupal.nodeRecipes.edges.map(edge => edge.node)
     recipes.forEach(recipedata => {
       createPage({

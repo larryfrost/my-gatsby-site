8e2eebe1bfff23bdcfa5171f20f300596003177c
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-06 12:03:22 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,14 +19,14 @@ exports.createPages = async ({ graphql, actions }) => {
     `);
 
     const articles = result.data.Drupal.nodeArticles.nodes;
-    const articleTemone = component: path.resolve('src/pages/article/articles/one.js'),
-    const articleTemtwo = component: path.resolve('src/pages/article/articles/two.js'),
-    const articleTemthree = component: path.resolve('src/pages/article/articles/three.js'),
-    const articleTemfour = component: path.resolve('src/pages/article/articles/four.js'),
-    const articleTemfive = component: path.resolve('src/pages/article/articles/five.js'),
-    const articleTemsix = component: path.resolve('src/pages/article/articles/six.js'),
-    const articleTemseven = component: path.resolve('src/pages/article/articles/seven.js'),
-    const articleTemeight = component: path.resolve('src/pages/article/articles/eight.js'),
+    const articleTemone = path.resolve('src/pages/article/articles/one.js'),
+    const articleTemtwo = path.resolve('src/pages/article/articles/two.js'),
+    const articleTemthree = path.resolve('src/pages/article/articles/three.js'),
+    const articleTemfour = path.resolve('src/pages/article/articles/four.js'),
+    const articleTemfive = path.resolve('src/pages/article/articles/five.js'),
+    const articleTemsix = path.resolve('src/pages/article/articles/six.js'),
+    const articleTemseven = path.resolve('src/pages/article/articles/seven.js'),
+    const articleTemeight = path.resolve('src/pages/article/articles/eight.js'),
       
     articles.forEach(articledata => {
       createPage({
@@ -92,16 +92,16 @@ exports.createPages = async ({ graphql, actions }) => {
     }
     `);
     const recipes = result2.data.Drupal.nodeRecipes.edges.map(edge => edge.node)
-    const recipeTemone = component: path.resolve('src/pages/recipe/one.js'),
-    const recipeTemtwo = component: path.resolve('src/pages/recipe/two.js'),
-    const recipeTemthree = component: path.resolve('src/pages/recipe/three.js'),
-    const recipeTemfour = component: path.resolve('src/pages/recipe/four.js'),
-    const recipeTemfive = component: path.resolve('src/pages/recipe/five.js'),
-    const recipeTemsix = component: path.resolve('src/pages/recipe/six.js'),
-    const recipeTemseven = component: path.resolve('src/pages/recipe/seven.js'),
-    const recipeTemeight = component: path.resolve('src/pages/recipe/eight.js'),
-    const recipeTemnine = component: path.resolve('src/pages/recipe/nine.js'),
-    const recipeTemten = component: path.resolve('src/pages/recipe/ten.js'),
+    const recipeTemone = path.resolve('src/pages/recipe/one.js'),
+    const recipeTemtwo = path.resolve('src/pages/recipe/two.js'),
+    const recipeTemthree = path.resolve('src/pages/recipe/three.js'),
+    const recipeTemfour = path.resolve('src/pages/recipe/four.js'),
+    const recipeTemfive = path.resolve('src/pages/recipe/five.js'),
+    const recipeTemsix = path.resolve('src/pages/recipe/six.js'),
+    const recipeTemseven = path.resolve('src/pages/recipe/seven.js'),
+    const recipeTemeight = path.resolve('src/pages/recipe/eight.js'),
+    const recipeTemnine = path.resolve('src/pages/recipe/nine.js'),
+    const recipeTemten = path.resolve('src/pages/recipe/ten.js'),
     recipes.forEach(recipedata => {
       createPage({
         path: recipedata.path,

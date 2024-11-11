e8ae9909da7c126046ec302680e1b3d2de31195e
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 14:34:27 -0500

    Update recipe.js

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,1 +1,47 @@
+import * as React from 'react';
+import Layout from '../../components/layout';
+import Seo from '../../components/seo';
+import { graphql } from 'gatsby';
 
+const RecipePage = ({ pageContext }) => {
+  const { id } = pageContext
+  const recipe = data.Drupal.nodeRecipes.edges.find(({ node }) => node.id === id).node;
+  return (
+    <Layout>
+      <h1>{recipe.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: recipe.summary.processed }}></div>
+      <div dangerouslySetInnerHTML={{ __html: recipe.recipeInstruction.value }}></div>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+query Recipes {
+  Drupal {
+    nodeRecipes(first: 10) {
+      edges {
+        node {
+          changed
+          id
+          cookingTime
+          created
+          path
+          status
+          preparationTime
+          numberOfServings
+          summary {
+            processed
+          }
+          recipeInstruction{
+            value
+          }
+          title
+        }
+      }
+    }
+  }
+}`;
+
+export const Head = () => <Seo title="Recipe Page" />
+
+export default RecipePage

8a61fe42a3c632f02388a1ae14ada169555cf8ed
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 16:08:02 -0500

    Update article.js

--- pages/template/article.js
+++ pages/template/article.js
@@ -1,35 +1,21 @@
 import * as React from 'react'
-import Layout from '../../../components/layout'
-import Seo from '../../../components/seo'
-import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
-const ArticlePage = ({ pageContext }) => {
-  const { id } = pageContext
-  const article = data.Drupal.nodeArticles.edges.find(({ node }) => node.id === id).node;
+const ArticlePage = ({ data }) => {
   return(
     <Layout>
-      <h1>{article.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: article.body.processed }}></div>
+       <article><h2><a href="/pages/article/one">Give it a go and grow your own herbs</a></h2></article>
+       <article><h2><a href="pages/article/two">Dairy-free and delicious milk chocolate</a></h2></article>
+       <article><h2><a href="pages/article/three"> The real deal for supermarket savvy shopping</a></h2></article>
+       <article><h2><a href="pages/article/four">The Umami guide to our favorite mushrooms</a></h2></article>
+       <article><h2><a href="pages/article/five">Let's hear it for carrots</a></h2></article>
+       <article><h2><a href="pages/article/six">Baking mishaps - our troubleshooting tips</a></h2></article>
+       <article><h2><a href="pages/article/seven">Skip the spirits with delicious mocktails</a></h2></article>
+       <article><h2><a href="pages/article/eight">Give your oatmeal the ultimate makover</a></h2></article>
     </Layout>
   );  
 };
-
-export const query = graphql`
-query Articles {
-  Drupal {
-    nodeArticles (first: 10) {
-      edges{
-        node{
-          id
-          title
-          body {
-            processed
-          }
-        }
-    }
-    }
-  }
-}`;
 
 export const Head = () => <Seo title="Article Page" />
 

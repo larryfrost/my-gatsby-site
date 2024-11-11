908e0c26749520753c9e655f9d46dc9106a5d44b
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 14:33:55 -0500

    Update article.js

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,1 +1,36 @@
+import * as React from 'react'
+import Layout from '../../../components/layout'
+import Seo from '../../../components/seo'
+import { graphql } from 'gatsby'
 
+const ArticlePage = ({ pageContext }) => {
+  const { id } = pageContext
+  const article = data.Drupal.nodeArticles.edges.find(({ node }) => node.id === id).node;
+  return(
+    <Layout>
+      <h1>{article.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: article.body.processed }}></div>
+    </Layout>
+  );  
+};
+
+export const query = graphql`
+query Articles {
+  Drupal {
+    nodeArticles (first: 10) {
+      edges{
+        node{
+          id
+          title
+          body {
+            processed
+          }
+        }
+    }
+    }
+  }
+}`;
+
+export const Head = () => <Seo title="Article Page" />
+
+export default ArticlePa

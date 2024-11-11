bd7d0332902a46b3275d2ffee258cee5f6e2505c
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 12:35:01 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,23 +9,19 @@ module.exports = {
       resolve: "gatsby-source-filesystem", 
       options: {
         name: `blog`,
-        path: `${__dirname}/blog/`,
+        path: `C:/Users/lfros/my_gatsby_site/blog`,
       },
     },
     "gatsby-plugin-mdx",
-      {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain remote schema Query type
-        typeName: "DrupalGraqhQL",
-        // This is field under which it's accessible 
-        fieldName: "Drupal",
-        // Url to query from
-        // url: "http://kecatalog.dev.wwbtc.com/graphql",
-        //  url: `http://10.89.0.11/graphql`,
-        // url: `https://api.keeneland.com/graphql`,
-        // url: "http://10.89.0.24/graphql",
-        url: `https://csc496f24demo.tldr.dev/graphql`,
+{
+  resolve: "gatsby-source-graphql",
+    options: {
+      // This type will contain remote schema Query type
+      typeName:  "DrupalGraphQL", // Fixed an additional typo
+      // This is field under which it's accessible
+      fieldName: "Drupal",
+      // Url to query from
+        url: "https://csc496f24demo.tldr.dev/graphql",
       },
     },
   ],

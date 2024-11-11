6e8bcff3d359ae25505e31c8658280b72fc7b02c
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 17:04:29 -0500

    Update one.js

--- src/pages/article/one.js
+++ src/pages/article/one.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import Layout from '../../../components/layout'
-import Seo from '../../../components/seo'
+import Layout from '../../src/components/layout'
+import Seo from '../../src/components/seo'
 import { graphql } from 'gatsby'
 
 const ArticlePage = ({ data }) => {

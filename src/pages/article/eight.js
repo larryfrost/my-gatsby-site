0b7d1119a9baa183cddfb2fe30e44cb385c898e7
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-11 16:28:45 -0500

    Add files via upload

---
+++ src/pages/article/eight.js
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import Layout from '../../../components/layout'
+import Seo from '../../../components/seo'
+import { graphql } from 'gatsby'
+
+const ArticlePage = ({ data }) => {
+  const articles = data.Drupal.nodeArticles.nodes;
+  const filteredArticle = articles.find(article => article.title === "Give your oatmeal the ultimate makeover"); 
+
+  return(
+    <Layout>
+      <h1>{filteredArticle.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: filteredArticle.body.processed }}></div>
+    </Layout>
+  );  
+};
+
+export const query = graphql`
+query Articles {
+  Drupal {
+    nodeArticles (first: 10) {
+      nodes{
+        id
+        title
+        body {
+          processed
+        }
+      }
+    }
+  }
+}`;
+
+export const Head = () => <Seo title="Article Page" />
+
+export default ArticlePage;

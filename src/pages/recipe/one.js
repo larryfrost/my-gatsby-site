1f0163ac709df54daf68d42b7d5f9017601bc7f4
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-13 17:49:08 -0500

    Update one.js

--- src/pages/recipe/one.js
+++ src/pages/recipe/one.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 import { graphql } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image';
 
 const RecipePage = ({ data }) => {
   const recipes = data.Drupal.nodeRecipes.edges.map(edge => edge.node);

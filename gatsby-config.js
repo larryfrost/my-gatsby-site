f0d154dcbab23764173c7301a1deda9d1ee8b625
larryfrost <157515725+larryfrost@users.noreply.github.com>
2024-11-13 17:22:41 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem", 
       options: {
